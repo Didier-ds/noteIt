8ece0102ed98f331f82364564148a534edfb5a16
didier-ds <didiersenou94@gmail.com>
2021-10-21 07:39:28 +0100

    first commit added firebase

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="view" >
+    <div id="code_box" class="sm:w-8/12 w-11/12 relative p-4 bg-gray-600 rounded-md mx-auto">
+      <div id="top_icons" class="absolute p-4 left-0 top-0">
+        <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle><circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle><circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle></g></svg>
+      </div>
+      <div>
+        <textarea v-model="note" @input="save" class="w-full h-full font-medium" autofocus autocorrect="off"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import {db, ref, set} from '@/utils/firebase.js'
+export default {
+  name: 'App',
+  data(){
+    return {
+      note: ''
+    }
+  },
+  methods:{
+    save(){
+      const userId = 0
+      console.log(this.note)
+     set(ref(db, `/users/${userId}`),{
+        note : this.note
+      })
+    }
+  },
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    console.log(ref)
+  },
+}
+</script>
+
+<style lang="scss">
+@import 'assets/styles/main.css';
+#view {
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background: #E7E8F2;
+}
+#code_box {
+  height: 80vh;
+  background-color:rgb(1, 22, 39) !important;
+  box-shadow: rgb(0 0 0 / 55%) 0px 20px 68px;
+}
+#code_box textarea {
+  background: transparent;
+  color: white;
+  margin-top: 2em;
+  min-height: 70vh;
+  resize: none;
+  color:rgb(130, 170, 255) !important;
+  border: none;
+  outline: none;
+}
+</style>
---
+++ src/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+// import { getStorage, ref } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAywirXRMZfY-Mp51T80C5bl4ebYsyd1Y",
+    authDomain: "noteit-7didier.firebaseapp.com",
+    databaseURL: "https://noteit-7didier-default-rtdb.firebaseio.com",
+    projectId: "noteit-7didier",
+    storageBucket: "noteit-7didier.appspot.com",
+    messagingSenderId: "550104670249",
+    appId: "1:550104670249:web:8979b9d1b92797b6467c38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+
+export { app, db, ref, set };
